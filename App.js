2418e000fbc2bd5200659a194e4fd5be6ec56414
Umut Aydın <AhmetUmutAydin@gmail.com>
2020-04-30 17:33:57 +0300

    Update App.js

--- App.js
+++ App.js
@@ -8,7 +8,6 @@ import {
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
-//space-between tüm boşlukları  arasına koyuyor, flexdirection yanyana mı üstüne mi gelcek vs..
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
   const [isAddMode,setIsAddMode]=useState(false);
@@ -33,7 +32,6 @@ export default function App() {
     setIsAddMode(false);
   }
 
-  //eğer () eklersen onChangeText'e fonksyion execute olur hemen ui render olduğunda biz key basıldığından istedğimiz için sadece ismini yazarız
   return (
     <View style={styles.screen}>
       <Button title="Add New Goal" onPress={()=> setIsAddMode(true)}/> 
@@ -44,23 +42,10 @@ export default function App() {
         renderItem={(itemData) => <GoalItem item={itemData.item} onDelete={removeGoalHandler}/>}
       />
 
-      {/* <ScrollView> 
-        {courseGoals.map((goal) => (
-          <View style={styles.listItem} key={goal}>
-            <Text >{goal}</Text>
-          </View>
-        ))}
-       </ScrollView>  */}
-      {/* Belli sayıda item varsa scrolView kullanılabilir ama sonsuzsa verimli değil.Flastlistte keyi kendi objenin içinden alıyor varsa arrayda*/}
     </View>
   );
 }
-//Viewların hertürlü flexboxu var ve içindekileri düzenliyor
-//main axis-> flexdirection'un
-//cross-axis -> flexdirection tersi  row->column row-reverse-column-reverse
-//justifyContent->main  alignItems->cross
-//flexbox içinde flex verip bölebiliriz main-axis te flexle bölme oluyor
-//strech alignItemse kapla demek
+
 const styles = StyleSheet.create({
   screen: {
     padding: 50,
